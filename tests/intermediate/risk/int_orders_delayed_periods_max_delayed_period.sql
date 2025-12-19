3b0be1a18f322f7977330399356421cfd69e2935
Alex Pérez <aveftw@gmail.com>
2025-12-19 19:26:06 +0000

    test: simplify max delayed_period validation using int_orders_delayed_periods

--- tests/intermediate/risk/int_orders_delayed_periods_max_delayed_period.sql
+++ tests/intermediate/risk/int_orders_delayed_periods_max_delayed_period.sql
@@ -2,49 +2,25 @@
 -- does not exceed the days_unbalanced value from the original order.
 -- If this query returns rows, the test will fail.
 
-with max_delayed_period_per_order as (
+with orders_delayed_periods as (
 
-    select
-        order_id,
-        shopper_id,
-        merchant_id,
-        product_id,
-        max(delayed_period) as max_delayed_period
-
-    from {{ ref('int_orders_delayed_periods') }}
-    group by
-        order_id,
-        shopper_id,
-        merchant_id,
-        product_id
+    select * from {{ ref('int_orders_delayed_periods') }}
 
 ),
 
-orders_days_unbalanced as (
+max_delayed_period_per_order as (
 
     select
         order_id,
-        shopper_id,
-        merchant_id,
-        product_id,
-        days_unbalanced
+        days_unbalanced,
+        max(delayed_period) as max_delayed_period
 
-    from {{ ref('int_orders') }}
+    from orders_delayed_periods
+    group by
+        order_id,
+        days_unbalanced
 
 )
 
-select
-    m.order_id,
-    m.shopper_id,
-    m.merchant_id,
-    m.product_id,
-    m.max_delayed_period,
-    o.days_unbalanced
-
-from max_delayed_period_per_order m
-join orders_days_unbalanced o
-    on m.order_id = o.order_id
-   and m.shopper_id = o.shopper_id
-   and m.merchant_id = o.merchant_id
-   and m.product_id = o.product_id
-where m.max_delayed_period > o.days_unbalanced
+select * from max_delayed_period_per_order
+where max_delayed_period > days_unbalanced
